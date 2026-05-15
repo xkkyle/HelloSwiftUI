27fc3efca13b5984622548f4ef56f1d38bb0e845
kyle-air <kylekwon.dev@gmail.com>
2026-05-15 18:53:37 +0900

    learn how to use navigationBarItems, navigationBarHidden, navigationBarBackButtonHidden, and NavigationLink to show Navigation, such as UINavigationController in UIKit

--- HelloSwiftUI/NavigationViews.swift
+++ HelloSwiftUI/NavigationViews.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct NavigationViews: View {
+
+    
     var body: some View {
-        NavigationView {
-            Image(systemName: "cart.fill").navigationBarTitle("네비게이션 바 타이틀", displayMode: .inline)
+        let leadingItem = Button(action : { print("leading item tapped") }) {
+            Image(systemName: "bell").imageScale(.large)
+        }
+        
+        let middleItem = Button(action : { print("middle Item Tapped") }) {
+            Image(systemName: "cart").imageScale(.large)
+        }
+        
+        let trailingItem = Button(action : { print("trailing item tapped") }) {
+            Image(systemName: "gear").imageScale(.large)
+        }
+        
+        let navigationBarItems = HStack {
+            leadingItem
+            middleItem
+            trailingItem
+        }
+        
+        let destination = VStack {
+            Text("Destination View")
+            Spacer()
+            Text("Destination View")
+            Spacer()
+            Text("Destination View")
+        }.navigationBarBackButtonHidden(true)
+        
+        return NavigationView {
+            NavigationLink(destination: destination) {
+                Image(systemName: "cart").renderingMode(.original)
+            }
+            .buttonStyle(DefaultButtonStyle())
+            .navigationBarItems(trailing: navigationBarItems)
+            .navigationBarTitle("네비게이션 타이틀", displayMode:.automatic)
+            .navigationBarHidden(true)
         }
     }
 }
